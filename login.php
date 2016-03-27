c0b96b5cd49935a92e77064d0e1b86a8f64e6ef3
kevin <a.kevin.schumacher@gmail.com>
2016-03-27 15:07:28 -0700

    removed of dupes

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <?php 
 	session_start(); 
@@ -47,53 +46,3 @@
 ?>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<?php 
-	session_start(); 
-	require "connection.php";
-?>
-<html>
-<head>
-<title>validating...</title>
-</head>
-<body>
-<?php
-	//User passed in variables
-	$email = filter_input(INPUT_POST, 'email');
-	$password = filter_input(INPUT_POST, 'password');;
-	
-	//Get connection
-    	$con = getConnection();
-	
-	//query the database to see if email in use.
-	$sql = "SELECT * FROM User WHERE email = '".$email."' AND password = PASSWORD('".$password."')";
-	$result = mysqli_query($con,$sql) or die(mysqli_error($con));
-	
-	if (mysqli_num_rows($result) == 1)
-	{
-		$fname = "";
-		$lname = "";
-    
-		//if authorized, get the values of FirstName LastName
-		while ($info = mysqli_fetch_array($result)) 
-		{
-			$fname = stripslashes($info['fname']);
-			$lname = stripslashes($info['lname']);
-		}
-		
-		//set session items for user to be used throughout website
-		$_SESSION["fname"] = $fname;
-		$_SESSION["lname"] = $lname;
-		$_SESSION["email"] = $email;
-				
-		//set authorization cookie
-		setcookie("auth", "1", time()+60*30, "/", "", 0);
-				
-		header("Location: home_page.php");
-		exit;
-	}
-?>
-</body>
-</html>
->>>>>>> refs/remotes/origin/master
--- registerpage.php
+++ registerpage.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <?php session_start(); ?>
 <html>
@@ -60,67 +59,3 @@
             </form>
         </div>
     </body>
-=======
-<!DOCTYPE html>
-<?php session_start(); ?>
-<html>
-    <head>
-        <title>Sign up</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <style>
-            body
-            {
-                background-color: powderblue;
-                font-size: 14pt;
-            }
-            
-            #page
-            {
-                padding-top: 200px;
-            }
-            
-            input[type=text], input[type=password], 
-            input[type=email], input[type=number]
-            {
-                width: 230px;
-                padding: 5px 5px;
-                margin-bottom: 10px;
-                display: inline-block;
-                border: 2px solid #000;
-                border-radius: 4px;
-            }
-            
-            input[type=submit]
-            {
-                padding: 10px 20px;
-                background-color: cadetblue;
-                color: white;
-                border: none;
-                border-radius: 4px;
-                cursor: pointer;
-                font-size: 13pt;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="page" align="center">
-            <form method="post" action="registerpage2.php" style="width:30%">
-                <fieldset>
-                    <legend>Register</legend>
-                    <input type="email" name="email" placeholder="Email" autofocus required/><br/>
-                    <!-- TODO PHP Validation of password -->
-                    <input type="password" name="password" placeholder="Password" required/><br/>
-                    <input type="password" name="cpassword" placeholder="Confirm Password" required/><br/><br/>
-                    <input type="text" name="fname" placeholder="First Name" required/><br/>
-                    <input type="text" name="lname" placeholder="Last Name" required/><br/>
-                    <input type="text" name="major" placeholder="Major" required/><br/>
-                    Year: <input style="width: 150px;" type="number" min="1" max="4" name="year" value="1"/><br/>
-                    <input type="submit" value="submit" name="Submit" align="center"/>
-                </fieldset>
-            </form>
-        </div>
-    </body>
->>>>>>> refs/remotes/origin/master
-</html>
