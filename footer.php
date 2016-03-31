906306d8eda5d26af91afec6f225c44d76678898
LamNg <LamNg96@gmail.com>
2016-03-31 15:41:35 -0700

    Header and Footer Created

---
+++ footer.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <style>
+            footer {
+                background-color:#2e51a2;
+                color:white;
+                clear:both;
+                text-align:center;
+                padding:5px;
+                bottom: 0;
+                position: absolute;
+                width: 100%;
+                overflow: hidden;
+            }
+        </style>
+    </head>
+    <body>
+
+
+        <footer>
+            Copyright
+        </footer>
+    </body>
+</html>
