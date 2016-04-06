8db774c8a2de2dc5a3fb516d436d82d49c9bc28d
Ileri Oyedele <ilerioyedele@gmail.com>
2016-04-06 13:01:30 -0700

    Update my_info_edit.php
    Gave title edit info

--- my_info_edit.php
+++ my_info_edit.php
@@ -36,7 +36,7 @@ $major = substr($degree, strpos($degree, ",") + 2);
 
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>Edit Info</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
